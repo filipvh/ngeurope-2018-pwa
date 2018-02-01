9d02da459ab938a1025c0384c793bd69da9bc3b2
SOF\fiva <fiva@sofico.be>
2018-02-01 16:52:44 +0100

    fix(registerPushNotification): user take(1) to unsubscribe the stream

--- client/src/modules/shared/services/pushNotification.service.ts
+++ client/src/modules/shared/services/pushNotification.service.ts
@@ -1,7 +1,9 @@
 import {Injectable} from '@angular/core';
 import {SwPush} from '@angular/service-worker';
 import {Observable} from 'rxjs/Observable';
+import { take } from 'rxjs/operators';
 import {ApiService} from './base/api.service';
+import {environment} from '../../../environments/environment';
 import {of} from 'rxjs/observable/of';
 import {fromPromise} from 'rxjs/observable/fromPromise';
 
@@ -12,14 +14,17 @@ export class PushNotificationService {
     constructor(private _swPush: SwPush, private _apiService: ApiService) {
     }
 
-    public register(): void {
+    public async register(): Promise<void> {
+        console.log('Push:', this._swPush.isEnabled);
         if (!this._swPush.isEnabled) {
-            console.log('Push: ', this._swPush.isEnabled);
             return;
         }
 
-        // Key generation: https://web-push-codelab.glitch.me/
-        // TODO
+        // Key generation: https://web-push-codelab.glitch.me
+        const subscription = await this._swPush.requestSubscription({ serverPublicKey: environment.push.publicKey});
+        console.log(subscription);
+        this._pushSubscription = subscription;
+        this._apiService.post('push/register', subscription).pipe(take(1)).subscribe();
     }
 
     public unregister(): Observable<boolean> {
